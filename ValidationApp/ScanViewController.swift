fe6a62c69471001ae2ea65a38bbb491351cd62b2
Dominik Mocher <dominik.mocher@a-sit.at>
2021-08-12 13:44:38 +0200

    Adapt to new validation core version

--- ValidationApp/ScanViewController.swift
+++ ValidationApp/ScanViewController.swift
@@ -11,7 +11,7 @@ import ValidationCore
 class ScanViewController: UIViewController {
     var result : ValidationResult?
     var error : ValidationError?
-    var validationCore = ValidationCore()
+    var validationCore = ValidationCore(trustlistService: nil, dateService: nil, trustlistUrl: nil, signatureUrl: nil, trustAnchor: nil)
     
     @IBOutlet weak var qrView: UIView!
     @IBOutlet weak var errorLabel: UILabel!
@@ -31,6 +31,7 @@ class ScanViewController: UIViewController {
         errorLabel.isHidden = true
         startScan()
     }
+    
     @IBAction func onUpdateTrustlist(_ sender: Any) {
         updateTrustlistButton.isEnabled = false
         errorLabel.isHidden = true
@@ -74,24 +75,67 @@ class ScanViewController: UIViewController {
                     self.performSegue(withIdentifier: "showResult", sender: nil)
                 }
             }
-            
-//            switch(result) {
-//            case .success(let validationResult):
-//                self.result = validationResult
-//                DispatchQueue.main.async {
-//                    self.performSegue(withIdentifier: "showResult", sender: nil)
-//                }
-//            case .failure(let error):
-//                DispatchQueue.main.async {
-//                    self.error = error
-//                    self.performSegue(withIdentifier: "showResult", sender: nil)
-//                }
-//            }
         }
     }
-    
-    private func show(errorMessage: String?) {
+
+    /*
+    private func testBusinessRules() {
+        var sigRequest = URLRequest(url: URL(string: "https://dgc.a-sit.at/ehn/rules/v1/sig")!)
+        sigRequest.addValue("application/octet-stream", forHTTPHeaderField: "Accept")
+        var rulesRequest = URLRequest(url: URL(string: "https://dgc.a-sit.at/ehn/rules/v1/bin")!)
+        rulesRequest.addValue("application/octet-stream", forHTTPHeaderField: "Accept")
+        
+        URLSession.shared.dataTask(with: sigRequest) { sigData, response, error in
+            guard error == nil,
+                  let response = response as? HTTPURLResponse,
+                  response.statusCode == 200,
+                  let sigData = sigData else {
+               return
+            }
+            URLSession.shared.dataTask(with: rulesRequest) { rulesData, response, error in
+                       guard error == nil,
+                             let response = response as? HTTPURLResponse,
+                             response.statusCode == 200,
+                             let rulesData = rulesData else {
+                          return
+                       }
+                 let decoded = try? self.validationCore.decode(businessRules: rulesData, signature: sigData, trustAnchor: "MIIBJTCBy6ADAgECAgUAwvEVkzAKBggqhkjOPQQDAjAQMQ4wDAYDVQQDDAVFQy1NZTAeFw0yMTA0MjMxMTI3NDhaFw0yMTA1MjMxMTI3NDhaMBAxDjAMBgNVBAMMBUVDLU1lMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE/OV5UfYrtE140ztF9jOgnux1oyNO8Bss4377E/kDhp9EzFZdsgaztfT+wvA29b7rSb2EsHJrr8aQdn3/1ynte6MSMBAwDgYDVR0PAQH/BAQDAgWgMAoGCCqGSM49BAMCA0kAMEYCIQC51XwstjIBH10SN701EnxWGK3gIgPaUgBN+ljZAs76zQIhAODq4TJ2qAPpFc1FIUOvvlycGJ6QVxNXEkhRcgdlVfUb")
+                 print(decoded)
+                   }.resume()
+        }.resume()
     }
     
+    private func testValueset() {
+        var sigRequest = URLRequest(url: URL(string: "https://dgc.a-sit.at/ehn/values/v1/sig")!)
+        sigRequest.addValue("application/octet-stream", forHTTPHeaderField: "Accept")
+        var rulesRequest = URLRequest(url: URL(string: "https://dgc.a-sit.at/ehn/values/v1/bin")!)
+        rulesRequest.addValue("application/octet-stream", forHTTPHeaderField: "Accept")
+        
+        URLSession.shared.dataTask(with: sigRequest) { sigData, response, error in
+            guard error == nil,
+                  let response = response as? HTTPURLResponse,
+                  response.statusCode == 200,
+                  let sigData = sigData else {
+               return
+            }
+            URLSession.shared.dataTask(with: rulesRequest) { valueSetData, response, error in
+                       guard error == nil,
+                             let response = response as? HTTPURLResponse,
+                             response.statusCode == 200,
+                             let valueSetData = valueSetData else {
+                          return
+                       }
+                let decoded = try? self.validationCore.decode(valueSet: valueSetData, signature: sigData, trustAnchor: """
+                    -----BEGIN CERTIFICATE-----
+                                                              MIIBJTCBy6ADAgECAgUAwvEVkzAKBggqhkjOPQQDAjAQMQ4wDAYDVQQDDAVFQy1NZTAeFw0yMTA0MjMxMTI3NDh
+                                                              aFw0yMTA1MjMxMTI3NDhaMBAxDjAMBgNVBAMMBUVDLU1lMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE/OV5Uf
+                                                              YrtE140ztF9jOgnux1oyNO8Bss4377E/kDhp9EzFZdsgaztfT+wvA29b7rSb2EsHJrr8aQdn3/1ynte6MSMBAwD
+                                                              gYDVR0PAQH/BAQDAgWgMAoGCCqGSM49BAMCA0kAMEYCIQC51XwstjIBH10SN701EnxWGK3gIgPaUgBN+ljZAs76
+                                                              zQIhAODq4TJ2qAPpFc1FIUOvvlycGJ6QVxNXEkhRcgdlVfUb
+                                                              -----END CERTIFICATE-----
+""")
+                print(decoded)
+                   }.resume()
+        }.resume()
+    }*/
 }
-
